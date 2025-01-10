99db81d7ab47547015f20df0354cec798caf0b7c
ChamkhiAnas <mahmoudbennouna@gmail.com>
2025-01-10 14:47:33 +0100

    Adding tranfsormations to the interceptors

--- src/users/interceptors/user.interceptor.ts
+++ src/users/interceptors/user.interceptor.ts
@@ -1,12 +1,15 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
-import { Observable } from "rxjs";
+import { Observable, map } from "rxjs";
+import { User } from "../types/User";
 
 @Injectable()
 export class UsersInterceptor implements NestInterceptor{
 
     intercept(context: ExecutionContext, next: CallHandler<any>):Observable<any>  | Promise <Observable<any>>{
         console.log("interceptor"+context.getClass().name)
-        return next.handle()       
+        return next.handle().pipe(map((data)=>data.map(
+            ({password,...user})=>user
+        )))       
     }
 
 }
